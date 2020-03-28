bff32a379416241eef8cb8a950a826a1cd2a56fc
Reverlight <artem3766@gmail.com>
2020-03-28 14:56:47 +0200

    Create DOM elements using value from select options html value.

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 window.addEventListener('load', () => {
+    const options = document.querySelectorAll('option');
+
+    const optionValues = [];
+
+    options.forEach(element => {
+        optionValues.push(element.innerHTML);
+    });
+
+
+    console.log(optionValues);
+
+    const customSelect = document.createElement('DIV');
+    const optionSelected = document.createElement('DIV');
+    const optionItems = document.createElement('DIV');
+
+
+
+    customSelect.setAttribute('id', 'custom-select');
+    customSelect.setAttribute('name', 'custom-restaurant-menu"')
+    optionSelected.setAttribute('id', 'option-selected');
+    optionItems.setAttribute('id', 'option-items');
+
+    document.body.appendChild(customSelect);
+    customSelect.appendChild(optionSelected);
+    customSelect.appendChild(optionItems);
+
+    optionValues.forEach((optionValue) => {
+        let customOption = document.createElement('DIV');
+        customOption.innerText = optionValue;
+        optionItems.append(customOption);
+    })
+
+
+
+
+    const firstOptionValue = optionValues[0];
+    optionSelected.innerHTML = firstOptionValue;
+    // optionSelected.appendChild(firstOption);
+
 
 })
